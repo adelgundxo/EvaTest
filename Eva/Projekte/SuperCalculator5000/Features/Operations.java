fc369b189cdb7b268122946afb4825983bdc9991
Eva Plutta <plutta@pondus.de>
2018-08-09 12:58:51 +0200

    AZUBI-8 IDEA PROJECT

--- Eva/Projekte/SuperCalculator5000/Features/Operations.java
+++ Eva/Projekte/SuperCalculator5000/Features/Operations.java
@@ -1,46 +1,48 @@
 package Features;
+
 import java.util.*;
+
 public class Operations {
- private String s;
- private double sum;
- private int amountOfNumbers;
- 
+  private String s;
+  private double sum;
+  private int amountOfNumbers;
 
-   public Operations() {
-        s = "Welcome to SuperCalculator5000!";
+
+  public Operations() {
+    s = "Welcome to SuperCalculator5000!";
+  }
+
+
+  public void doSomeMath(Map<Integer, Double> map) {
+    amountOfNumbers = 0;
+
+    for (Map.Entry<Integer, Double> counter : map.entrySet()) {
+      amountOfNumbers++;
     }
-    
-    
-public void doSomeMath(Map<Integer,Double> map){
-amountOfNumbers = 0;
+    System.out.println(amountOfNumbers + "Numbers added.");
+    sum(map);
 
-    for(Map.Entry<Integer, Double> counter : map.entrySet()){
-    amountOfNumbers++;
+  }
+
+  private void sum(Map<Integer, Double> map) {
+
+    sum = 0.0;
+
+    for (Map.Entry<Integer, Double> pairs : map.entrySet()) {
+
+      sum += pairs.getValue();
+
     }
-System.out.println(amountOfNumbers + "Numbers added.");
-sum(map);
+    this.print(sum);
+  }
 
-}
+  private void print(double sum) {
 
-private void sum(Map<Integer, Double> map){
+    System.out.println(s);
+    System.out.println("This is the sum of many or not so many random values: " + sum);
 
-sum = 0.0;
+  }
 
-for(Map.Entry<Integer,Double> pairs : map.entrySet()){
-
-    sum += pairs.getValue();
-   
-}
- this.print(sum);
-}
-
-private void print(double sum){
-
-System.out.println(s);
-System.out.println("This is the sum of many or not so many random values: " + sum);
-
-}
- 
 
 }
 
---
+++ Eva/Projekte/SuperCalculator5000/SuperCalculator5000/SC.java
@@ -0,0 +1,30 @@
+package SuperCalculator5000;
+
+import java.util.*;
+
+import Features.*;
+
+public class SC {
+
+
+  public static void main(String[] args) {
+
+    int Min = 0;
+    int Max = 20;
+    int randomAmount = Min + (int) (Math.random() * ((Max - Min) + 1));
+    double num;
+    Map<Integer, Double> map = new HashMap<>();
+
+    for (int i = 0; i < randomAmount; i++) {
+
+      num = Math.random();
+      map.put(i, num);
+
+    }
+
+    Operations operation = new Operations();
+    operation.doSomeMath(map);
+
+  }
+
+}
